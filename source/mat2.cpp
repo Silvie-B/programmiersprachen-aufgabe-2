7e1744d392f5a713b0a508876ca2c25c27009163
Silvie Böning <silvie.boening@t-online>
2018-05-06 10:49:03 +0200

    Add Mat2

--- source/mat2.cpp
+++ source/mat2.cpp
@@ -1,6 +1,35 @@
 #include "mat2.hpp"
 
-struct mat2{
-    mat2();
-    mat2(
+
+Mat2::Mat2():
+x1(1.0),
+x2(0.0),
+y1(0.0),
+y2(1.0) {}
+
+Mat2::Mat2(float x1, float x2, float y1, float y2):
+x1(x1),
+x2(x2),
+y1(y1),
+y2(y2) {}
+
+Mat2& Mat2::operator*=(Mat2 const& m)
+{
+    x1 *= m.x1;
+    x2 *= m.x2;
+    y1 *= m.y1;
+    y2 *= m.y2;
+    return *this;
 }
+
+Mat2 operator*(Mat2 const& m1, Mat2 const& m2)
+{
+    Mat2 *m3 = new Mat2();
+    
+    m3->x1 = m1.x1 * m2.x1 + m1.x2 * m2.y1;
+    m3->x2 = m1.x1 * m2.x2 + m1.x2 * m2.y2;
+    m3->y1 = m1.y1 * m2.x1 + m1.y2 * m2.y1;
+    m3->y2 = m1.y1 * m2.x2 + m1.y2 * m2.y2;
+    return *m3;
+}
+
--- source/mat2.hpp
+++ source/mat2.hpp
@@ -1,6 +1,14 @@
 #ifndef mat2_hpp
 #define mat2_hpp
 
-
+struct Mat2{
+    float x1;
+    float x2;
+    float y1;
+    float y2;
+    Mat2();
+    Mat2(float x1, float x2, float y1, float y2);
+    Mat2& operator*=(Mat2 const& m);
+};
 
 #endif 
